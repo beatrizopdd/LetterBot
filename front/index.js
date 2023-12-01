7dbda47e036d5f380392115ca4666ab20c999270
Beatriz <100521126+beatrizopdd@users.noreply.github.com>
2023-12-01 20:40:23 -0300

    att

---
+++ front/index.js
@@ -0,0 +1,14 @@
+let site = document.URL;
+console.log(site);
+
+let titulo = document.title;
+console.log(titulo);
+
+let conteiner = document.getElementsByClassName("container");
+console.log(conteiner);
+
+let a = document.getElementsByTagName("a");
+console.log(a);
+
+let p = document.getElementsByTagName("p");
+console.log(p.innerHTML);
